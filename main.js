e143dbb8de8275d294833feaf2790e6d677e2d77
whitehatvarun <86660633+whitehatvarun@users.noreply.github.com>
2021-11-07 10:19:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    
+}
+
+function draw()
+{
+   
+}
+
+function take_snapshot()
+{
+ save('myMoustacheFilter.png');
+}
